394d2b8314d5b4062156b003bfb7e6acbdce338c
Tiina Vir <tinsku78@gmail.com>
2019-06-14 10:10:42 +0300

    Initial commit

---
+++ AzureStorageConfig.cs
@@ -0,0 +1,15 @@
+﻿//24.5.2019
+//huom! ei githubiin
+//save images to azure blob 
+//falvstoragaAccesskeys
+using System;
+
+namespace falvStorage
+{
+    public class AzureStorageConfig
+    {
+        public string AccountName = "#Accountname";
+        public string AccountKey = "#Accountkey";
+        public string ImageContainer = "#Imagecontainer";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿//24.5.2019
+//save images to azure blob
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace falvStorage
+{
+    class CfalvStorage
+    {
+        static async Task Main(string[] args)
+        {
+            const string receiptImage = @"C:\kuitti1.jpg";
+
+            using (Stream imageStream = File.OpenRead(receiptImage))
+            {
+                await UploadFileToStorage(imageStream, "kuva", new AzureStorageConfig());
+            }
+
+
+
+        }//end main
+
+
+
+        public static async Task<bool> UploadFileToStorage(Stream fileStream, string fileName, AzureStorageConfig _storageConfig)
+        {
+            // Create storagecredentials object by reading the values from the configuration (appsettings.json)
+            StorageCredentials storageCredentials = new StorageCredentials(_storageConfig.AccountName, _storageConfig.AccountKey);
+
+            // Create cloudstorage account by passing the storagecredentials
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Get reference to the blob container by passing the name by reading the value from the configuration (appsettings.json)
+            CloudBlobContainer container = blobClient.GetContainerReference(_storageConfig.ImageContainer);
+
+            // Get the reference to the block blob from the container
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+
+            // Upload the file
+            await blockBlob.UploadFromStreamAsync(fileStream);
+
+            return await Task.FromResult(true);
+        }
+    } //end class
+} //end namespace
